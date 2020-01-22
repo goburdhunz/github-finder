3ea4a64c11732ac3b54a0f578624cd6735e3f078
zeeshan goburdhun <zgoburdhun@wundermancommerce.com>
2020-01-22 16:15:19 +0000

    added details and repo list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Alert from './components/Alert'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
+import About from './components/pages/About'
 import axios from 'axios'
 import './App.css'
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
     alert: null
   }
@@ -23,6 +28,7 @@ class App extends Component {
   //   this.setState({ users: res.data, loading: false })
   // }
 
+  // search github users based on search text
   searchUsers = async text => {
     this.setState({ loading: true })
 
@@ -33,8 +39,32 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false })
   }
 
+  // Go to a single Github user
+  getUser = async login => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(
+      `https://api.github.com/users/${login}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+
+    this.setState({ user: res.data, loading: false })
+  }
+
+  // get users repos
+  getUserRepos = async login => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(
+      `https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+
+    this.setState({ repos: res.data, loading: false })
+  }
+
+  // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false })
 
+  // Set alerts
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } })
 
@@ -42,22 +72,49 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading } = this.state
+    const { users, user, repos, loading } = this.state
 
     return (
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     )
   }
 }
