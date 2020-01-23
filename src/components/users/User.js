4a83ee03c95c4881c9d402326a499359ec6b6254
zeeshan goburdhun <zgoburdhun@wundermancommerce.com>
2020-01-23 16:54:29 +0000

    refactored alerts using seperate context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { useEffect, Fragment } from 'react'
+import React, { useContext, useEffect, Fragment } from 'react'
 import Repos from '../repos/Repos'
 import Spinner from '../Spinner'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import GithubContext from '../../context/github/githubContext'
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext)
+
+  const { getUser, loading, user, repos, getUserRepos } = githubContext
+
   useEffect(() => {
     getUser(match.params.login)
     getUserRepos(match.params.login)
@@ -95,14 +99,6 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
       <Repos repos={repos} />
     </Fragment>
   )
-}
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
-  repos: PropTypes.array.isRequired
 }
 
 export default User
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -34,8 +34,30 @@ const GithubState = props => {
   }
 
   // Get user
+  const getUser = async login => {
+    setLoading()
+
+    const res = await axios.get(
+      `https://api.github.com/users/${login}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    })
+  }
 
   // Get Repos
+  const getUserRepos = async login => {
+    setLoading()
+
+    const res = await axios.get(
+      `https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    })
+  }
 
   // Clear Users
   const clearUsers = () => dispatch({ type: CLEAR_USERS })
@@ -51,7 +73,9 @@ const GithubState = props => {
         repos: state.repos,
         loading: state.loading,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}
